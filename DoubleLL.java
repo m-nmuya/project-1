f14d035fd4435214a58859072ec087d589a40413
MaanasaRamakrishnan <149803524+MaanasaRamakrishnan@users.noreply.github.com>
2024-09-17 19:52:38 -0400

    Update DoubleLL.java

--- DoubleLL.java
+++ DoubleLL.java
@@ -1,11 +1,19 @@
+
 /**
  * Doubly Linked List class
  *
- * @author Maanasa Ramakrishnan (maanasar)
-           Marie Muya (mariem26)
+ * @author Maanasa Ramakrishnan (maanasar) Marie Muya (mariem26)
  * @version 2024.09.20
  */
 
-public class DoubleLL {
+public class DoubleLL
+{
+    private static class DLLNode<E>
+    {
+        private E data;
+        private DLLNode<E> prev;
+        private DLLNode<E> next;
+       
 
+    }
 }
