0a774e2a7588245d840e625c7be5d3972baa26c3
MaanasaRamakrishnan <149803524+MaanasaRamakrishnan@users.noreply.github.com>
2024-09-17 20:10:05 -0400

    Update Node.java

--- Node.java
+++ Node.java
@@ -6,6 +6,44 @@
  * @version 2024.09.20
  */
 
-public class Node {
+ public class Node<D>
+    {
 
-}
+        // The data element stored in the node.
+        public D data;
+
+        // The next node in the sequence.
+        public Node<D> next;
+
+        /**
+         * Creates a new node with the given data
+         *
+         * @param d
+         *            the data to put inside the node
+         */
+        public Node(D d)
+        {
+            data = d;
+        }
+               
+        /**
+         * Sets the node after this node
+         *
+         * @param n
+         *            the node after this one
+         */
+        public void set(Node<D> n)
+        {
+            next = n;
+        }
+
+        /**
+         * Gets the data in the node
+         *
+         * @return the data in the node
+         */
+        public D get()
+        {
+            return data;
+        }
+    }
