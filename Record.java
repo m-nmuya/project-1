676f5b12be20806438738d189ab6570be46b20a1
Marie Muya <161767231+m-nmuya@users.noreply.github.com>
2024-09-18 17:57:12 -0400

    Update Record.java

--- Record.java
+++ Record.java
@@ -7,5 +7,30 @@
  */
 
 public class Record {
-
+    // ~ Fields ................................................................
+    private String key;
+    private Node node;
+    
+    // ~ Constructor ...........................................................
+    public Record(String key, Node node) {
+        this.key = key;
+        this.node = node;
+    }
+    
+    // ~ Public Methods ........................................................
+    public String getKey() {
+        return key;
+    }
+    
+    public Node getNode() {
+        return node;
+    }
+    
+    public void setKey(String key) {
+        this.key = key;
+    }
+    
+    public void setNode(Node node) {
+        this.node = node;
+    }
 }
