ab864bac8e624fd2ede267656719dc7ae41a3d90
Marie Muya <161767231+m-nmuya@users.noreply.github.com>
2024-09-21 16:35:06 -0400

    Update Record.java

--- Record.java
+++ Record.java
@@ -1,36 +1,33 @@
 /**
- * Record class
- *
- * @author Maanasa Ramakrishnan (maanasar)
-           Marie Muya (mariem26)
- * @version 2024.09.20
- */
+* Record class
+*
+* @author Maanasa Ramakrishnan (maanasar)
+*         Marie Muya (mariem26)
+* @version 2024.09.20
+*/
 
 public class Record {
-    // ~ Fields ................................................................
-    private String key;
-    private Node node;
-    
-    // ~ Constructor ...........................................................
-    public Record(String key, Node node) {
-        this.key = key;
-        this.node = node;
-    }
-    
-    // ~ Public Methods ........................................................
-    public String getKey() {
-        return key;
-    }
-    
-    public Node getNode() {
-        return node;
-    }
-    
-    public void setKey(String key) {
-        this.key = key;
-    }
-    
-    public void setNode(Node node) {
-        this.node = node;
-    }
+   private String key;
+   private Node node;
+
+   public Record(String key, Node node) {
+       this.key = key;
+       this.node = node;
+   }
+
+   public String getKey() {
+       return key;
+   }
+
+   public void setKey(String key) {
+       this.key = key;
+   }
+
+   public Node getNode() {
+       return node;
+   }
+
+   public void setNode(Node node) {
+       this.node = node;
+   }
 }
